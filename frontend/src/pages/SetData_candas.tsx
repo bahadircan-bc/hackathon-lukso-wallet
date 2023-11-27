0550c915272a3687f364311e567eb437df7b2f58
Bahadır Can <bahadircan.bc@gmail.com>
2023-11-27 07:48:30 +0300

    set data implemented

--- frontend/src/pages/SetData_candas.tsx
+++ frontend/src/pages/SetData_candas.tsx
@@ -1,51 +1,77 @@
 import Web3 from "web3";
-import LSP0ERC725Account from "@lukso/lsp-smart-contracts/artifacts/LSP0ERC725Account.json";
+import UniversalProfile from "@lukso/lsp-smart-contracts/artifacts/UniversalProfile.json";
 import { useState } from "react";
 import { AbiItem } from "web3-utils";
 
 const web3 = new Web3(window.lukso);
-console.log(web3.eth)
 
-export default function SetData_candas() {
-  const [twitterHandle, setTwitterHandle] = useState("");
+export default function SetGetInstagramHandle() {
+  const [instagramHandle, setInstagramHandle] = useState("");
+  const [retrievedHandle, setRetrievedHandle] = useState("");
 
-  const updateTwitterHandle = async (handle: string, address: string) => {
+  const updateInstagramHandle = async (handle: string, address: string) => {
     try {
       const contract = new web3.eth.Contract(
-        LSP0ERC725Account.abi as AbiItem[],
-        address // Replace with actual contract address
+        UniversalProfile.abi as AbiItem[],
+        address // Replace with the Universal Profile contract address
       );
 
-      const key = web3.utils.keccak256("TwitterHandle");
+      const key = web3.utils.keccak256("InstagramHandle");
       const value = web3.utils.asciiToHex(handle);
-
-      const receipt = await contract.methods
-        .setData(key, value)
-        .send({ from: address, gas: "1000000" });
+      const receipt = await contract.methods.setData(key, value).send({
+        from: address,
+        gas: "1000000",
+      });
       console.log("Transaction successful:", receipt);
     } catch (error) {
-      console.error("Error:", error);
+      console.error("Error updating Instagram handle:", error);
+    }
+  };
+
+  const getInstagramHandle = async (address: string) => {
+    try {
+      const contract = new web3.eth.Contract(
+        UniversalProfile.abi as AbiItem[],
+        address // Replace with the Universal Profile contract address
+      );
+
+      const key = web3.utils.keccak256("InstagramHandle");
+      const data = await contract.methods.getData(key).call();
+      const handle = web3.utils.hexToAscii(data);
+      setRetrievedHandle(handle);
+      console.log("Instagram Handle:", handle);
+    } catch (error) {
+      console.error("Error fetching Instagram handle:", error);
     }
   };
 
   const connectAndSetHandle = async () => {
     try {
       const accounts = await web3.eth.requestAccounts();
-      console.log('Connected account:', accounts[0]);
-      await updateTwitterHandle(twitterHandle, accounts[0]);
+      await updateInstagramHandle(instagramHandle, accounts[0]);
     } catch (error) {
       console.error("Error connecting:", error);
     }
   };
 
+  const connectAndGetHandle = async () => {
+    try {
+      const accounts = await web3.eth.requestAccounts();
+      await getInstagramHandle(accounts[0]);
+    } catch (error) {
+      console.error("Error connecting:", error);
+    }
+  };
   return (
-    <div>
+    <div className="flex flex-col gap-5 px-[35%]">
       <input
         type="text"
-        value={twitterHandle}
-        onChange={(e) => setTwitterHandle(e.target.value)}
+        value={instagramHandle}
+        onChange={(e) => setInstagramHandle(e.target.value)}
       />
-      <button onClick={connectAndSetHandle}>Set Twitter Handle</button>
+      <button onClick={connectAndSetHandle}>Set Instagram Handle</button>
+      <button onClick={connectAndGetHandle}>Get Instagram Handle</button>
+      {retrievedHandle && <p>Retrieved Instagram Handle: {retrievedHandle}</p>}
     </div>
   );
 }
